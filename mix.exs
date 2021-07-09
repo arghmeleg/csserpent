a654f97478814e47b86ba7cca409432c4c6c41db
Steve DeGele <arghmeleg@users.noreply.github.com>
2021-07-09 01:14:48 -0400

    package stuff

--- mix.exs
+++ mix.exs
@@ -1,13 +1,19 @@
 defmodule CSSerpent.MixProject do
   use Mix.Project
 
+  @description "CSSerpent is a CSS parsing library."
+  @version "0.1.0"
+
   def project do
     [
       app: :csserpent,
-      version: "0.1.0",
+      version: @version,
+      description: @description,
       elixir: "~> 1.8",
+      package: package(),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      source_url: "https://github.com/arghmeleg/csserpent"
     ]
   end
 
@@ -21,8 +27,24 @@ defmodule CSSerpent.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
+  end
+
+  defp package do
+    %{
+      maintainers: ["Steve DeGele"],
+      licenses: ["MIT"],
+      files: [
+        "lib",
+        "test",
+        "mix.exs",
+        "README.md",
+        "LICENSE"
+      ],
+      links: %{
+        "GitHub" => "https://github.com/arghmeleg/csserpent"
+      }
+    }
   end
 end
